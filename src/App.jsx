a6d6d3b8deb4017a9c7b3c5fc6b0d9373508eebe
mellcosta <m3licosta@gmail.com>
2025-01-08 16:33:40 +0530

    :lipstick: Setting Background Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import Contact from "./Components/Contact"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+import Projects from "./Components/Projects"
+import Tech from "./Components/Tech"
+
 function App() {
   return (
     <>
-      <div className="text-8xl font-bold">
-        Hello World
-      </div>
+      <div className="fixed -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+
+      <main className="flex flex-col items-center px-4 md:px-8 lg:px-16 ">
+        <Navbar />
+        <Hero/>
+        <Tech/>
+        <Projects/>
+        <Contact/>
+        
+      </main>
     </>
   )
 }
