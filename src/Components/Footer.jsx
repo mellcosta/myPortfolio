df8f7125cc4775920722984a2c2cbcc074548925
mellcosta <m3licosta@gmail.com>
2025-01-09 20:20:42 +0530

    :sparkles: Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <footer className=" bottom-0 flex w-full items-center justify-between border-b border-b-gray-700 bg-black/70 px-16 py-6 text-white backdrop-blur-md md:justify-evenly " >
+        <p className="text-gray-400">
+              @2025
+          </p>
+      </footer>
+    )
+}
+
+export default Footer
